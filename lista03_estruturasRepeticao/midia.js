7a321f90fd7f7c695e7672c11567abdbae308363
Amábile Silva <123522722+Anne2023@users.noreply.github.com>
2023-03-01 20:10:36 -0300

    a

--- lista03_estruturasRepeticao/midia.js
+++ lista03_estruturasRepeticao/midia.js
@@ -1,23 +1,25 @@
-var btn  = document.getElementById('btnCalcular')
-	btn.addEventListener("click", mdc")
+var btn = document.getElementById('btnCalcular')
+btn.addEventListener("click",executar)
 
-function mdc( ){
-		let n1 = parseInt(document.getElementById('n1').value)
-		let n2 = parseInt(document.getElementById('n2').value)
-        let, result = document.getElementById('result')
-        
+function executar(){
+    let n1 = parseInt(document.getElementById('n1').value)
+    let n2= parseInt(document.getElementById('n2').value)
+    let result = document.getElementById('result')
+    
 
-        if(n1>n2){
-            for(let ctrl=n2;ctrl>n1;ctrl -- )
-                if(n1%ctrl==0 && n2%ctrl==0)
-                    result.innerText=ctrl
-                    break
-
-        }else{
-            for(let ctrl=n1;ctrl>n1;ctrl -- )
-                if(n1%ctrl==0 && n2%ctrl==0)
-                    result.innerText=ctrl
-                    break
-
+    if(n1>n2){
+        for(let ctrl=n2;ctrl>=1;ctrl--){
+            if(n1%ctrl=0 && n2%ctrl==0){
+                result.innerText=ctrl
+                break
+            }
         }
+    }else{
+        for(let ctrl=n1;ctrl>=1;ctrl--){
+            if(n1%ctrl==0 && n2%ctrl==0){
+                result.innerText=ctrl
+                break
+            }
+        }
+    }
 }
