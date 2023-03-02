b2de51ef5bee08d00086f1ae4c371083a2731139
Amábile Silva <123522722+Anne2023@users.noreply.github.com>
2023-03-01 21:49:00 -0300

    a

--- lista03_estruturasRepeticao/midia.js
+++ lista03_estruturasRepeticao/midia.js
@@ -1,25 +1,28 @@
 var btn = document.getElementById('btnCalcular')
 btn.addEventListener("click",executar)
 
-var result = document.getElementById('result')
+
 
 
 function executar(){
     let n1 = parseInt(document.getElementById('n1').value)
     let n2= parseInt(document.getElementById('n2').value)
-    
+    var result = document.getElementById('result')
+
     if(n1>n2){
-        mdc(n1,n2)
+        result.innerText= mdc(n1,n2)
     }else{
-        mdc(n2,n1)
+        result.innerText= mdc(n2,n1)
     }
 }
 
 function mdc(maior, menor){
     for(let ctrl=menor;ctrl>=1;ctrl--){
         if(maior%ctrl==0 && menor%ctrl==0){
-            result.innerText=ctrl
-            break
+            return ctrl
         }
     }
+}
+function mdc(maior, menor){
+    while(menor!=0)
 }
